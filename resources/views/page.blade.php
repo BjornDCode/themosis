8d7511eda6e549a68f099a30d64d8e50bca1819c
Bjorn Lindholm <bjornlindholmhansen@gmail.com>
2020-01-21 11:00:09 +0100

    Setup simple Page template

---
+++ resources/views/page.blade.php
@@ -0,0 +1,5 @@
+<h1>{{ $page->post_title }}</h1>
+
+<main>
+    {!! $page->post_content !!}
+</main>
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,15 @@
 /**
  * Application routes.
  */
+
+// Single Page Routes
+Route::any('page', function ($page) {
+    return view('page', [
+        'page' => $page,
+    ]);
+});
+
+// Front Page
 Route::get('/', function () {
     return view('welcome');
 });
